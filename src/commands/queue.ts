fa35868f3cf99b9efb487997324fd96942f9e17a
dablenparty <hdavenport8902@gmail.com>
2022-08-06 02:28:22 -0400

    Fixed queue pagination

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -18,17 +18,23 @@ const command: CloverCommand = {
       const fromArgs = parseInt(args[0]);
       if (!isNaN(fromArgs)) page = fromArgs - 1;
     }
-    page = Math.max(0, Math.min(page, maxPages - 1)) * 10;
+    // saturate pages between 0 and maxPages
+    // multiply by 10 because there are (max) 10 songs per page
+    // add one for the song that is currently playing
+    page = Math.max(0, Math.min(page, maxPages - 1)) * 10 + 1;
+    const nowPlaying = queue.songs[0];
     const thisPage = queue.songs.slice(page, page + 10);
-    const q = thisPage
-      .map((song, i) => `${i === 0 ? "Playing:" : `${i + page}.`} \`${song.name}\` - \`${song.formattedDuration}\``)
+    // TODO: show time left
+    let queueString = `**Now playing:** ${nowPlaying.name}\n\n**Up next:**\n`;
+    queueString += thisPage
+      .map((song, i) => `${i + page}) \`${song.name}\` - \`${song.formattedDuration}\``)
       .join("\n");
     message.channel.send({
       embeds: [
         new EmbedBuilder()
-          .setTitle(`Queue:`)
-          .setDescription(q)
-          .setFooter({ text: `Page ${page / 10 + 1}/${maxPages}` })
+          .setTitle(`${message.guild?.name ?? "This Server"}'s Queue`)
+          .setDescription(queueString)
+          .setFooter({ text: `Page ${Math.floor(page / 10) + 1}/${maxPages}` })
           .setColor("#00ff00"),
       ],
     });
