7ff05defeb0d03aac0c0866751581453a49e4a55
dablenparty <hdavenport8902@gmail.com>
2022-08-22 22:59:15 -0400

    Removed unnecessary awaits on returned Promises

--- src/distube.ts
+++ src/distube.ts
@@ -3,6 +3,7 @@
 import { YtDlpPlugin } from "@distube/yt-dlp";
 import { Colors, EmbedBuilder } from "discord.js";
 import DisTube, { Queue } from "distube";
+import formatDuration from "format-duration";
 import config from "../config.json";
 import discordClient from "./discord";
 
@@ -51,7 +52,7 @@ distubeClient
   .on(
     "playSong",
     async (queue, song) =>
-      await queue.textChannel?.send({
+      queue.textChannel?.send({
         embeds: [
           embedStatus(queue, new EmbedBuilder())
             .setTitle(`Now Playing: ${song.name ?? "Unknown"} - ${formatDuration(song.duration * 1000)}`)
@@ -62,32 +63,28 @@ distubeClient
         ],
       }),
   )
-  .on(
-    "addSong",
-    async (queue, song) =>
-      await queue.textChannel?.send({
-        embeds: [
-          new EmbedBuilder()
-            .setTitle(`Added ${song.name ?? "Unknown"}`)
-            .setURL(song.url)
-            .setDescription(song.formattedDuration || "")
-            .setImage(song.thumbnail ?? null)
-            .setColor(Colors.Green),
-        ],
-      }),
+  .on("addSong", async (queue, song) =>
+    queue.textChannel?.send({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle(`Added ${song.name ?? "Unknown"}`)
+          .setURL(song.url)
+          .setDescription(song.formattedDuration || "")
+          .setImage(song.thumbnail ?? null)
+          .setColor(Colors.Green),
+      ],
+    }),
   )
-  .on(
-    "addList",
-    async (queue, playlist) =>
-      await queue.textChannel?.send({
-        embeds: [
-          embedStatus(queue, new EmbedBuilder())
-            .setTitle(`Added ${playlist.name} playlist`)
-            .setURL(playlist.url ?? null)
-            .setDescription(`${playlist.songs.length} songs`)
-            .setColor(Colors.Green),
-        ],
-      }),
+  .on("addList", async (queue, playlist) =>
+    queue.textChannel?.send({
+      embeds: [
+        embedStatus(queue, new EmbedBuilder())
+          .setTitle(`Added ${playlist.name} playlist`)
+          .setURL(playlist.url ?? null)
+          .setDescription(`${playlist.songs.length} songs`)
+          .setColor(Colors.Green),
+      ],
+    }),
   )
   .on("error", async (channel, e) => {
     if (channel)
@@ -101,25 +98,19 @@ distubeClient
       });
     else console.error(e);
   })
-  .on(
-    "empty",
-    async (queue) =>
-      await queue.textChannel?.send({
-        embeds: [new EmbedBuilder().setTitle("I don't stay in empty voice channels").setColor(Colors.Red)],
-      }),
+  .on("empty", async (queue) =>
+    queue.textChannel?.send({
+      embeds: [new EmbedBuilder().setTitle("I don't stay in empty voice channels").setColor(Colors.Red)],
+    }),
   )
-  .on(
-    "searchNoResult",
-    async (message, query) =>
-      await message.channel.send({
-        embeds: [new EmbedBuilder().setTitle(`No results for "${query}"!`).setColor(Colors.Red)],
-      }),
+  .on("searchNoResult", async (message, query) =>
+    message.channel.send({
+      embeds: [new EmbedBuilder().setTitle(`No results for "${query}"!`).setColor(Colors.Red)],
+    }),
   )
-  .on(
-    "finish",
-    async (queue) =>
-      await queue.textChannel?.send({
-        embeds: [new EmbedBuilder().setTitle("Queue finished, leaving...").setColor(Colors.Green)],
-      }),
+  .on("finish", async (queue) =>
+    queue.textChannel?.send({
+      embeds: [new EmbedBuilder().setTitle("Queue finished, leaving...").setColor(Colors.Green)],
+    }),
   );
 export default distubeClient;
